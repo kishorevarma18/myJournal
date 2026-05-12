3d5b33ec2bd48e6e8d0c91e1ad43a7e70117fc4d
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-05-12 15:51:13 -0400

    renamed the file

---
+++ src/main/java/com/jkv/myJournal/entity/JournalEntriesWithoutDb.java
@@ -0,0 +1,26 @@
+package com.jkv.myJournal.entity;
+
+public class JournalEntriesWithoutDb {
+    private Long id;
+    private String name;
+    private String content;
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
