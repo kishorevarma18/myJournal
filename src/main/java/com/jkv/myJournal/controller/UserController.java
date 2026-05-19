fd339b43e24f974171fd79602c05fe905e605655
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-05-19 17:33:12 -0400

    made some mapping changes

--- src/main/java/com/jkv/myJournal/controller/UserController.java
+++ src/main/java/com/jkv/myJournal/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.jkv.myJournal.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,8 @@ import com.jkv.myJournal.entity.UserEntity;
 import com.jkv.myJournal.service.UserService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -21,56 +24,61 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
-@RequestMapping("/Users")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public ResponseEntity<?> getUsers(){
-        if(!userService.getAll().isEmpty())
-            return ResponseEntity.status(HttpStatus.OK).body(userService.getAll());
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("no data");    
-    }
-
-    @PostMapping
-    public ResponseEntity<?> saveUsers(@RequestBody UserEntity userEntity) {
-        if(userEntity!=null){
-            userService.saveAll(userEntity);
-            return ResponseEntity.status(HttpStatus.OK).body(userEntity);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Body can't be null");
-    }   
+    // @GetMapping
+    // public ResponseEntity<?> getUsers(){
+    //     if(!userService.getAll().isEmpty())
+    //         return ResponseEntity.status(HttpStatus.OK).body(userService.getAll());
+    //     return ResponseEntity.status(HttpStatus.NO_CONTENT).body("no data");    
+    // }   
     
-    @GetMapping({"/id/{myId}","/id"})
-    public ResponseEntity<?> getUserById(@PathVariable(required = false) String myId){
-        if(myId!=null && !myId.equals("")){
-            return ResponseEntity.status(HttpStatus.OK).body(userService.getById(myId));
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
-    }
+    // @GetMapping({"/id/{myId}","/id"})
+    // public ResponseEntity<?> getUserById(@PathVariable(required = false) String myId){
+    //     if(myId!=null && !myId.equals("")){
+    //         return ResponseEntity.status(HttpStatus.OK).body(userService.getById(myId));
+    //     }
+    //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
+    // }
 
-    @PutMapping({"id/{myId}","/id"})
-    public ResponseEntity<?> updateUserById(@PathVariable(required = false) String myId, @RequestBody UserEntity newUser) {
-        if(myId!=null && !myId.equals("")){
-            return ResponseEntity.status(HttpStatus.OK).body(userService.updateById(myId,newUser));
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
-    }
+    // @PutMapping({"id/{myId}","/id"})
+    // public ResponseEntity<?> updateUserById(@PathVariable(required = false) String myId, @RequestBody UserEntity newUser) {
+    //     if(myId!=null && !myId.equals("")){
+    //         return ResponseEntity.status(HttpStatus.OK).body(userService.updateById(myId,newUser));
+    //     }
+    //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
+    // }
 
-    @DeleteMapping({"id/{myId}","/id"})
-    public ResponseEntity<?> deleteUserById(@PathVariable(required = false) String myId){
-        if(myId!=null && !myId.equals("")){
-            return ResponseEntity.status(HttpStatus.OK).body(userService.deleteById(myId));
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
-    }
+    // @DeleteMapping({"id/{myId}","/id"})
+    // public ResponseEntity<?> deleteUserById(@PathVariable(required = false) String myId){
+    //     if(myId!=null && !myId.equals("")){
+    //         return ResponseEntity.status(HttpStatus.OK).body(userService.deleteById(myId));
+    //     }
+    //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id not available or null or empty");
+    // }
 
-    @PutMapping({"/{userName}","/"})
-    public ResponseEntity<?> putMethodName(@PathVariable(required = false) String userName, @RequestBody UserEntity newUser) {
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody UserEntity newUser) {
+        Authentication authenticatedUser=SecurityContextHolder.getContext().getAuthentication();
+        String userName = authenticatedUser.getName();
         if(userName!=null){
             return ResponseEntity.status(HttpStatus.OK).body(userService.updateByName(userName, newUser));
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("userName not valid or null or empty");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser(Principal principal){
+        String userName = principal.getName();
+        try{
+                return ResponseEntity.status(HttpStatus.OK).body(userService.deleteByName(userName));
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()); 
+        }
+        
     }
 }
