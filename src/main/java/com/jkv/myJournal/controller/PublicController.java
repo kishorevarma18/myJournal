302314305aa713138a24eedff0f3bc4c42ab3d43
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-05-19 17:28:25 -0400

    added the controller for public access.

---
+++ src/main/java/com/jkv/myJournal/controller/PublicController.java
@@ -0,0 +1,40 @@
+package com.jkv.myJournal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jkv.myJournal.entity.UserEntity;
+import com.jkv.myJournal.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/test")
+    public String test() {
+        return "test-running!";
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<?> saveUsers(@RequestBody UserEntity userEntity) {
+        if(userEntity!=null){
+            userService.saveAll(userEntity);
+            return ResponseEntity.status(HttpStatus.OK).body(userEntity);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Body can't be null");
+    }
+
+    
+}
