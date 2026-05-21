493e37e23565e77c95b019213eec35c62cbfc2de
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-05-21 10:17:09 -0400

    made changes to accommodate new methods.

--- src/main/java/com/jkv/myJournal/service/UserService.java
+++ src/main/java/com/jkv/myJournal/service/UserService.java
@@ -6,7 +6,8 @@ import java.util.Optional;
 import com.jkv.myJournal.entity.UserEntity;
 
 public interface UserService {
-    void saveAll(UserEntity userEntity);
+    void saveNewAll(UserEntity userEntity);
+    void saveUser(UserEntity userEntity);
     List<UserEntity> getAll();
     Optional<UserEntity> getById(String id);
     boolean deleteById(String id);
