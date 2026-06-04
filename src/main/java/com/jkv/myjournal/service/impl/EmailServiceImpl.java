c37e8d09ee96ecc594cc4401d2903d80d5b7b302
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-06-04 16:39:20 -0400

    implemented the sendEmail method.

---
+++ src/main/java/com/jkv/myjournal/service/impl/EmailServiceImpl.java
@@ -0,0 +1,37 @@
+package com.jkv.myjournal.service.impl;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.jkv.myjournal.service.EmailService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService{
+    private final JavaMailSender javaMailSender;
+
+    @Async
+    //using Async here so that this process can be handled by another thread, without blocking the main thread.
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            log.info("Sending email to: {}", to);
+            javaMailSender.send(message);
+            log.info("Email sent successfully to {}", to);
+        }
+        catch(Exception e){
+            log.error("Failed to send email to {}: {}", to, e.getMessage());
+        }
+    }
+
+}
